a4849ba7ddc2483b85015658d837a41be3a5484d
Maxime Pinard <maxime.pin@live.fr>
2016-12-03 11:55:02 +0100

    From UML to Java, starting implementation

---
+++ src/main/java/org/tiwindetea/magicmetro/model/Inventory.java
@@ -0,0 +1,95 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 Time Winder Dev Team (tiwindetea.org)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.tiwindetea.magicmetro.model;
+
+/**
+ * TODO
+ */
+public class Inventory {
+
+	/**
+	 * Default constructor
+	 */
+	public Inventory() {
+	}
+
+
+	/**
+	 * TODO
+	 */
+	public void addStationUpgrade() {
+		// TODO
+	}
+
+	/**
+	 * TODO
+	 */
+	public void addTrain() {
+		// TODO
+	}
+
+	/**
+	 * TODO
+	 */
+	public void addPassengerCar() {
+		// TODO
+	}
+
+	/**
+	 * TODO
+	 */
+	public void addLine() {
+		// TODO
+	}
+
+	/**
+	 * TODO
+	 */
+	public void removeTrain() {
+		// TODO
+	}
+
+	/**
+	 * TODO
+	 */
+	public void removeLine() {
+		// TODO
+	}
+
+	/**
+	 * TODO
+	 */
+	public void removePassengerCar() {
+		// TODO
+	}
+
+	/**
+	 * TODO
+	 */
+	public void removeStationUpgrade() {
+		// TODO
+	}
+
+}
