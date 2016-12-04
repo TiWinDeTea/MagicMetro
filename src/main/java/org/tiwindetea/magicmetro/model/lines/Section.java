adf2dc41a1773de7e66121032e621e6fbc9a61e5
Maxime Pinard <maxime.pin@live.fr>
2016-12-04 17:08:45 +0100

    Implementation of tunnel and length for Section

--- src/main/java/org/tiwindetea/magicmetro/model/lines/Section.java
+++ src/main/java/org/tiwindetea/magicmetro/model/lines/Section.java
@@ -24,18 +24,103 @@
 
 package org.tiwindetea.magicmetro.model.lines;
 
+import org.tiwindetea.magicmetro.global.util.Pair;
+import org.tiwindetea.magicmetro.global.util.SimplePair;
+import org.tiwindetea.magicmetro.model.Station;
+
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
- * TODO
+ * A section of a line composed of several connections and subsections.
+ * The two last connections are part of different stations linked by the section.
+ *
+ * @author Maxime PINARD
+ * @see SubSection
+ * @see Connection
+ * @see Line
+ * @since 0.1
  */
 public class Section {
 
+	private SimplePair<Connection> lastConnections;
+	private Collection<Connection> connections;
+	private Collection<SubSection> subSections;
 	private boolean hasTunnel;
-	public int length;
+	private boolean tunnelUpToDate = false;
+	private int length;
+	private boolean lengthUpToDate = false;
 
 	/**
-	 * Default constructor
+	 * Instantiates a new Section.
+	 *
+	 * @param connections     the connections
+	 * @param lastConnections the last connections
 	 */
-	public Section() {
+	public Section(@Nonnull Collection<Connection> connections, @Nonnull Pair<Connection, Connection> lastConnections) {
+
+		this.connections = new ArrayList<>(connections);
+		this.lastConnections = new SimplePair<>(lastConnections);
+	}
+
+	private void computeTunnel() {
+
+		this.hasTunnel = false;
+		for(SubSection subSection : this.subSections) {
+			if(subSection.isTunnel()) {
+				this.hasTunnel = true;
+				break;
+			}
+		}
+		this.tunnelUpToDate = true;
+	}
+
+	private void computeLength() {
+
+		this.length = 0;
+		for(SubSection subSection : this.subSections) {
+			this.length += subSection.getLength();
+		}
+		this.lengthUpToDate = true;
+	}
+
+	/**
+	 * Gets if has tunnel.
+	 *
+	 * @return true if has tunnel, false otherwise
+	 */
+	public boolean hasTunnel() {
+
+		if(!this.tunnelUpToDate) {
+			computeTunnel();
+		}
+		return this.hasTunnel;
+	}
+
+	/**
+	 * Gets length.
+	 *
+	 * @return the length
+	 */
+	public double getLength() {
+
+		if(!this.lengthUpToDate) {
+			computeLength();
+		}
+		return this.length;
+	}
+
+	/**
+	 * Gets stations linked by this section.
+	 *
+	 * @return the stations
+	 */
+	@Nonnull
+	public Pair<Station, Station> getStations() {
+
+		return new Pair<>(this.lastConnections.getLeft().getStationRef(),
+		  this.lastConnections.getRight().getStationRef());
 	}
 
 }
