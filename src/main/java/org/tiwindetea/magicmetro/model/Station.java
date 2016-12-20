cc6b1a920f252c2634d0f6bae4cf0a8875c3e914
Maxime Pinard <maxime.pin@live.fr>
2016-12-20 14:01:34 +0100

    Station implementation

--- src/main/java/org/tiwindetea/magicmetro/model/Station.java
+++ src/main/java/org/tiwindetea/magicmetro/model/Station.java
@@ -24,41 +24,112 @@
 
 package org.tiwindetea.magicmetro.model;
 
+import org.arakhne.afc.math.geometry.d2.d.Point2d;
+import org.tiwindetea.magicmetro.model.lines.Connection;
+import org.tiwindetea.magicmetro.view.StationView;
+
+import javax.annotation.Nonnull;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
- * TODO
+ * A station, contains connections and passengers.
+ *
+ * @author Maxime PINARD
+ * @since 0.1
  */
 public class Station {
 
-	private int maxCapacity;
-	//private StationView view;
+	private static final int STATION_DEFAULT_CAPACITY = 10; // TODO: real value
+	private static final int STATION_FULL_DELAY = 10; // TODO: real value
+	private static final int STATION_MAX_CONNECTIONS = 10; // TODO: real value
+
+	private final Point2d position;
+	private final StationType type;
+	private final StationView view;
+	private int maxCapacity = STATION_DEFAULT_CAPACITY;
+
+	private final List<Passenger> passengers = new LinkedList<>();
+	private final List<Connection> connections = new LinkedList<>();
 
 	/**
-	 * Default constructor
-	 */
-	public Station() {
-
-	}
-
-	/**
-	 * TODO
+	 * Instantiates a new Station.
 	 *
-	 * @param passenger TODO
+	 * @param position the position
+	 * @param type     the type
+	 * @param view     the view
 	 */
-	public void addPassenger(Passenger passenger) {
-		// TODO
+	public Station(@Nonnull Point2d position, @Nonnull StationType type, @Nonnull StationView view) {
+		this.position = position;
+		this.type = type;
+		this.view = view;
 	}
 
 	/**
-	 * TODO
+	 * Add a passenger.
 	 *
-	 * @param passenger TODO
+	 * @param passenger the passenger
 	 */
-	public void removePassenger(Passenger passenger) {
-		// TODO
+	public void addPassenger(@Nonnull Passenger passenger) {
+		this.passengers.add(passenger);
+		this.view.addPassenger(passenger.getWantedStation());
+		if(this.passengers.size() > this.maxCapacity) {
+			this.view.warn(STATION_FULL_DELAY);
+			//TODO: add station to a list in the game loop to check if the game is lost. (with starting time of the delay)
+		}
 	}
 
+
+	/**
+	 * Remove a passenger.
+	 *
+	 * @param passenger the passenger
+	 * @return true if the passenger was removed, false if the passenger is not in the station
+	 */
+	public boolean removePassenger(@Nonnull Passenger passenger) {
+		if(this.passengers.remove(passenger)) {
+			this.view.removePassenger(passenger.getWantedStation());
+			// if station was more than full before
+			if(this.passengers.size() == this.maxCapacity) {
+				this.view.unWard();
+				//TODO: remove station from game loop check list.
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Gets the station type.
+	 *
+	 * @return the station type
+	 */
+	@Nonnull
 	public StationType getType() {
-		//TODO
+		return this.type;
+	}
+
+	/**
+	 * Add a connection.
+	 *
+	 * @param connection the connection
+	 * @return true if the connection was added, false if the station already has the maximum connections
+	 */
+	public boolean addConnection(@Nonnull Connection connection) {
+		if(this.connections.size() < STATION_MAX_CONNECTIONS) {
+			this.connections.add(connection);
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Remove a connection.
+	 *
+	 * @param connection the connection
+	 * @return true if the connection was removed, false if the connection is not in the station
+	 */
+	public boolean removeConnection(@Nonnull Connection connection) {
+		return this.connections.remove(connection);
 	}
 
 }
