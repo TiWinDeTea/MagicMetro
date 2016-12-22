bd1cc36b461033b9f548ed4d887ca97dec888413
Maxime Pinard <maxime.pin@live.fr>
2016-12-22 17:19:03 +0100

    ConcreteStationView implmentation

---
+++ src/main/java/org/tiwindetea/magicmetro/view/ConcreteStationView.java
@@ -0,0 +1,112 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 TiWinDeTea - contact@tiwindetea.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.tiwindetea.magicmetro.view;
+
+import javafx.application.Platform;
+import javafx.scene.Parent;
+import javafx.scene.layout.TilePane;
+import javafx.scene.shape.Shape;
+import org.arakhne.afc.math.geometry.d2.d.Point2d;
+import org.tiwindetea.magicmetro.model.StationType;
+
+import javax.annotation.Nonnull;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * A JavaFx implementation of StationView.
+ *
+ * @author Maxime PINARD
+ * @since 0.1
+ */
+public class ConcreteStationView extends Parent implements StationView {
+
+	private static final int PASSENGER_COLUMNS = 6;
+	private static final int PASSENGER_OFFSET_X = 10;
+	private static final int PASSENGER_OFFSET_Y = 0;
+
+	private final Shape sprite;
+	private final List<PassengerView> passengers = new LinkedList<>();
+	private final PassengerViewFactory passengerViewFactory;
+	private final TilePane tilePane = new TilePane();
+
+	public ConcreteStationView(Shape sprite,
+	                           int spriteWidth,
+	                           int spriteHeight,
+	                           PassengerViewFactory passengerViewFactory) {
+		this.sprite = sprite;
+		this.getChildren().add(sprite);
+		this.sprite.setTranslateX(0);
+		this.sprite.setTranslateY(0);
+		this.setLayoutX(-spriteWidth / 2);
+		this.setLayoutY(-spriteHeight / 2);
+		this.passengerViewFactory = passengerViewFactory;
+
+		this.getChildren().add(this.tilePane);
+		this.tilePane.setPrefColumns(PASSENGER_COLUMNS);
+		this.tilePane.setTranslateX(spriteWidth + PASSENGER_OFFSET_X);
+		this.tilePane.setTranslateY(PASSENGER_OFFSET_Y);
+	}
+
+	@Override
+	public void setPosition(@Nonnull Point2d position) {
+		this.setTranslateX(position.getX());
+		this.setTranslateY(position.getY());
+	}
+
+	@Override
+	public void addPassenger(@Nonnull StationType wantedStation) {
+		PassengerView passenger = new PassengerView(wantedStation,
+		  this.passengerViewFactory.newPassengerView(wantedStation));
+		this.passengers.add(passenger);
+		Platform.runLater(() -> this.tilePane.getChildren().add(passenger));
+	}
+
+	@Override
+	public void removePassenger(@Nonnull StationType wantedStation) {
+		for(PassengerView passenger : this.passengers) {
+			if(passenger.getWantedStation() == wantedStation) {
+				this.passengers.remove(passenger);
+				Platform.runLater(() -> this.tilePane.getChildren().remove(passenger));
+				break;
+			}
+		}
+	}
+
+	@Override
+	public void makeBigger() {
+		//TODO
+	}
+
+	@Override
+	public void warn(int seconds) {
+		//TODO
+	}
+
+	@Override
+	public void unWard() {
+		//TODO
+	}
+}
