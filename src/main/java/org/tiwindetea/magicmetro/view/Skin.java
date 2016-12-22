5e441e3212ac57ee35fe5d67a6ca521ba69c3eb9
Maxime Pinard <maxime.pin@live.fr>
2016-12-22 16:15:32 +0100

    Skin implementation

---
+++ src/main/java/org/tiwindetea/magicmetro/view/Skin.java
@@ -0,0 +1,184 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 TiWinDeTea - contact@tiwindetea.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.tiwindetea.magicmetro.view;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Shape;
+import org.arakhne.afc.math.geometry.d2.d.Point2d;
+import org.tiwindetea.magicmetro.model.StationType;
+import org.tiwindetea.magicmetro.model.TrainType;
+
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Skin, give graphical element to the viewManager.
+ *
+ * @author Maxime PINARD
+ * @since 0.1
+ */
+public class Skin implements PassengerViewFactory {
+
+	//FIXME: temporary test implementation
+
+	public static final int TRAIN_VIEW_WIDTH = 150;
+	public static final int TRAIN_VIEW_HEIGHT = 80;
+
+	private static final Color PASSENGERS_COLOR = Color.GRAY;
+	private static final Color STATIONS_COLOR1 = Color.DARKGRAY;
+	private static final Color STATIONS_COLOR2 = Color.BLACK;
+	private static final Color ERROR_COLOR = Color.BLACK;
+	private static final List<Color> LINES_COLORS = new ArrayList<Color>() {{
+		add(Color.YELLOW);
+		add(Color.RED);
+		add(Color.BLUE);
+		add(Color.GREEN);
+		add(Color.PINK);
+		add(Color.CYAN);
+		add(Color.MAROON);
+	}};
+	private static final List<Point2d> TRAIN_PASSENGERS_POSITIONS = new ArrayList<Point2d>() {{
+		add(new Point2d(0, 0));
+		add(new Point2d(40, 0));
+		add(new Point2d(80, 0));
+		add(new Point2d(0, 40));
+		add(new Point2d(40, 40));
+		add(new Point2d(80, 40));
+	}};
+
+	@Override
+	@Nonnull
+	public Shape newPassengerView(@Nonnull StationType wantedStation) {
+		switch(wantedStation) {
+		case SQUARE: {
+			Polygon polygon = new Polygon();
+			polygon.getPoints().addAll(
+			  0.0, 0.0,
+			  40.0, 0.0,
+			  40.0, 40.0,
+			  0.0, 40.0
+			);
+			polygon.setFill(PASSENGERS_COLOR);
+			return polygon;
+		}
+		case TRIANGLE: {
+			Polygon polygon = new Polygon();
+			polygon.getPoints().addAll(
+			  20.0, 0.0,
+			  40.0, 40.0,
+			  0.0, 40.0
+			);
+			polygon.setFill(PASSENGERS_COLOR);
+			return polygon;
+		}
+		case CIRCLE: {
+			Circle circle = new Circle(20);
+			circle.setLayoutX(20);
+			circle.setLayoutY(20);
+			circle.setFill(PASSENGERS_COLOR);
+			return circle;
+		}
+		case STAR: {
+			//TODO
+			Circle circle = new Circle(20);
+			circle.setFill(ERROR_COLOR);
+			return circle;
+		}
+		case DIAMOND: {
+			Polygon polygon = new Polygon();
+			polygon.getPoints().addAll(
+			  20.0, 0.0,
+			  40.0, 20.0,
+			  20.0, 40.0,
+			  0.0, 20.0
+			);
+			polygon.setFill(PASSENGERS_COLOR);
+			return polygon;
+		}
+		default: // return undefined shape
+			break;
+		}
+		//TODO
+		Circle circle = new Circle(20);
+		circle.setFill(ERROR_COLOR);
+		return circle;
+	}
+
+	@Nonnull
+	public Shape newStationView(@Nonnull StationType type) {
+		Shape shape = newPassengerView(type);
+		shape.setScaleX(2);
+		shape.setScaleY(2);
+		shape.setFill(STATIONS_COLOR1);
+		shape.setStrokeWidth(5);
+		shape.setStroke(STATIONS_COLOR2);
+		return shape;
+	}
+
+	@Nonnull
+	public Shape newTrainView(@Nonnull TrainType type) {
+		//TODO: switch type
+		Polygon polygon = new Polygon();
+		polygon.getPoints().addAll(
+		  0.0, 0.0,
+		  120.0, 0.0,
+		  150.0, 40.0,
+		  120.0, 80.0,
+		  0.0, 80.0
+		);
+		polygon.setFill(ERROR_COLOR);
+		return polygon;
+	}
+
+	@Nonnull
+	public Shape newPassengerCarView(@Nonnull TrainType type) {
+		Polygon polygon = new Polygon();
+		polygon.getPoints().addAll(
+		  0.0, 0.0,
+		  120.0, 0.0,
+		  120.0, 80.0,
+		  0.0, 80.0
+		);
+		polygon.setFill(ERROR_COLOR);
+		return polygon;
+	}
+
+	@Nonnull
+	public Color getLineColor(int lineNumber) {
+		if(lineNumber < LINES_COLORS.size()) {
+			return LINES_COLORS.get(lineNumber);
+		}
+		return ERROR_COLOR;
+	}
+
+	@Nonnull
+	public List<Point2d> getTrainPassengerPositions() {
+		return TRAIN_PASSENGERS_POSITIONS;
+	}
+
+}
