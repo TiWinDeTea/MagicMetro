520638d9bfc7280cf970e25fe1ad57ec6f53b634
Maxime Pinard <maxime.pin@live.fr>
2016-12-29 11:03:00 +0100

    SectionView implementation

---
+++ src/main/java/org/tiwindetea/magicmetro/view/SectionView.java
@@ -0,0 +1,252 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 TiWinDeTea - contact@tiwindetea.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.tiwindetea.magicmetro.view;
+
+import javafx.scene.Parent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polyline;
+import org.arakhne.afc.math.geometry.d2.d.Point2d;
+import org.arakhne.afc.math.geometry.d2.dfx.MultiShape2dfx;
+import org.arakhne.afc.math.geometry.d2.dfx.Point2dfx;
+import org.arakhne.afc.math.geometry.d2.dfx.Rectangle2dfx;
+import org.arakhne.afc.math.geometry.d2.dfx.Segment2dfx;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * The view of a line section.
+ *
+ * @author Maxime PINARD
+ * @since 0.1
+ */
+public class SectionView extends Parent {
+
+	private ConcreteStationView fromStation = null;
+	private ConcreteStationView toStation = null;
+	private MultiShape2dfx<Rectangle2dfx> water = new MultiShape2dfx<>(); // for collisions
+	private boolean intersectWater = false;
+	private Polyline polyline;
+	private Point2dfx a;
+	private Point2dfx b;
+	private Point2dfx c;
+	private Segment2dfx ab;
+	private Segment2dfx bc;
+
+	private void updatePoints() {
+		if(Math.abs(this.c.getX() - this.a.getX()) > Math.abs(this.c.getY() - this.a.getY())) {
+			this.b.setX(this.a.getX()
+			  + (this.c.getX() - this.a.getX())
+			  - (this.c.getX() > this.a.getX() ? 1 : -1) * Math.abs(this.c.getY() - this.a.getY()));
+			this.b.setY(this.a.getY());
+		}
+		else {
+			this.b.setX(this.a.getX());
+			this.b.setY(this.a.getY()
+			  + (this.c.getY() - this.a.getY())
+			  - (this.c.getY() > this.a.getY() ? 1 : -1) * Math.abs(this.c.getX() - this.a.getX()));
+		}
+	}
+
+	private void updateWaterIntersection() {
+		this.ab.set(this.a, this.b);
+		this.bc.set(this.b, this.c);
+		if(this.ab.intersects(this.water) || this.bc.intersects(this.water)) {
+			this.intersectWater = true;
+			this.polyline.setStroke(Color.RED); //FIXME
+		}
+		else {
+			this.intersectWater = false;
+			this.polyline.setStroke(Color.BLUE); //FIXME
+		}
+	}
+
+	/**
+	 * Instantiates a new SectionView.
+	 *
+	 * @param fromX the x coordinate of section start point
+	 * @param fromY the y coordinate of section start point
+	 * @param toX   the x coordinate of section end point
+	 * @param toY   the y coordinate of section end point
+	 */
+	public SectionView(double fromX, double fromY, double toX, double toY) {
+		this.polyline = new Polyline();
+		this.polyline.getPoints().add(fromX);
+		this.polyline.getPoints().add(fromY);
+		this.polyline.getPoints().add(fromX);
+		this.polyline.getPoints().add(fromY);
+		this.polyline.getPoints().add(toX);
+		this.polyline.getPoints().add(toY);
+		this.a = new Point2dfx();
+		this.a.xProperty().addListener(((observable, oldValue, newValue) ->
+		  this.polyline.getPoints().set(0, newValue.doubleValue())));
+		this.a.yProperty().addListener(((observable, oldValue, newValue) ->
+		  this.polyline.getPoints().set(1, newValue.doubleValue())));
+		this.b = new Point2dfx();
+		this.b.xProperty().addListener(((observable, oldValue, newValue) ->
+		  this.polyline.getPoints().set(2, newValue.doubleValue())));
+		this.b.yProperty().addListener(((observable, oldValue, newValue) ->
+		  this.polyline.getPoints().set(3, newValue.doubleValue())));
+		this.c = new Point2dfx();
+		this.c.xProperty().addListener(((observable, oldValue, newValue) ->
+		  this.polyline.getPoints().set(4, newValue.doubleValue())));
+		this.c.yProperty().addListener(((observable, oldValue, newValue) ->
+		  this.polyline.getPoints().set(5, newValue.doubleValue())));
+		this.a.set(fromX, fromY);
+		this.b.set(fromX, fromY);
+		this.c.set(toX, toY);
+		this.ab = new Segment2dfx(this.a, this.b);
+		this.bc = new Segment2dfx(this.b, this.c);
+
+		this.polyline.setStroke(Color.BLUE); //FIXME
+		this.polyline.setStrokeWidth(20); //TODO: define ?
+		this.getChildren().add(this.polyline);
+
+		this.updatePoints();
+		this.updateWaterIntersection();
+	}
+
+	/**
+	 * Instantiates a new SectionView.
+	 *
+	 * @param from the section start point
+	 * @param to   the section end point
+	 */
+	public SectionView(Point2d from, Point2d to) {
+		this(from.getX(), from.getY(), to.getX(), to.getY());
+	}
+
+	/**
+	 * Instantiates a new SectionView.
+	 *
+	 * @param initX the x coordinate of section start and end point
+	 * @param initY the y coordinate of section start and end point
+	 */
+	public SectionView(double initX, double initY) {
+		this(initX, initY, initX, initY);
+	}
+
+	/**
+	 * Sets the section start point.
+	 *
+	 * @param x the x coordinate of section start point
+	 * @param y the y coordinate of section start point
+	 */
+	public void setFrom(double x, double y) {
+		this.a.xProperty().setValue(x);
+		this.a.yProperty().setValue(y);
+		this.updatePoints();
+		this.updateWaterIntersection();
+	}
+
+	/**
+	 * Sets the section start point.
+	 *
+	 * @param from the section start point
+	 */
+	public void setFrom(Point2d from) {
+		this.setFrom(from.getX(), from.getY());
+	}
+
+	/**
+	 * Sets the section end point.
+	 *
+	 * @param x the x coordinate of section end point
+	 * @param y the y coordinate of section end point
+	 */
+	public void setTo(double x, double y) {
+		this.a.xProperty().setValue(x);
+		this.a.yProperty().setValue(y);
+		this.updatePoints();
+		this.updateWaterIntersection();
+	}
+
+	/**
+	 * Sets the section end point.
+	 *
+	 * @param to the the section end point
+	 */
+	public void setTo(Point2d to) {
+		this.setTo(to.getX(), to.getY());
+	}
+
+	/**
+	 * Sets water for collisions.
+	 *
+	 * @param water the water
+	 */
+	public void setWater(@Nonnull MultiShape2dfx<Rectangle2dfx> water) {
+		this.water = water;
+		this.updateWaterIntersection();
+	}
+
+	/**
+	 * Determine if the section intersect the water.
+	 *
+	 * @return the boolean
+	 */
+	public boolean intersectWater() {
+		return this.intersectWater;
+	}
+
+	/**
+	 * Sets from station.
+	 *
+	 * @param fromStation the from station
+	 */
+	public void setFromStation(@Nullable ConcreteStationView fromStation) {
+		this.fromStation = fromStation;
+	}
+
+	/**
+	 * Gets from station.
+	 *
+	 * @return the from station
+	 */
+	@Nullable
+	public ConcreteStationView getFromStation() {
+		return this.fromStation;
+	}
+
+	/**
+	 * Sets to station.
+	 *
+	 * @param toStation the to station
+	 */
+	public void setToStation(@Nullable ConcreteStationView toStation) {
+		this.toStation = toStation;
+	}
+
+	/**
+	 * Gets to station.
+	 *
+	 * @return the to station
+	 */
+	@Nullable
+	public ConcreteStationView getToStation() {
+		return this.toStation;
+	}
+
+}
