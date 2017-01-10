29ea86c0ee069b14d545be12a61497254fd7cf79
Julien Barbier <barbier.julien@orange.fr>
2017-01-10 12:33:46 +0100

    Adding first version of Path-finding
    Implementing the new Path-finding.

--- src/main/java/org/tiwindetea/magicmetro/model/GameMap.java
+++ src/main/java/org/tiwindetea/magicmetro/model/GameMap.java
@@ -30,16 +30,13 @@ import org.tiwindetea.magicmetro.global.eventdispatcher.events.lineevents.LineCr
 import org.tiwindetea.magicmetro.global.eventdispatcher.events.lineevents.LineDecreaseEvent;
 import org.tiwindetea.magicmetro.global.eventdispatcher.events.lineevents.LineExtensionEvent;
 import org.tiwindetea.magicmetro.global.eventdispatcher.events.lineevents.LineInnerExtensionEvent;
+import org.tiwindetea.magicmetro.global.util.BooleanWrapper;
 import org.tiwindetea.magicmetro.model.lines.Connection;
 import org.tiwindetea.magicmetro.model.lines.Line;
 import org.tiwindetea.magicmetro.model.lines.Section;
 
 import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * Map of the game contains trains, stations and lines that are active in the game.<p>
@@ -57,6 +54,7 @@ public class GameMap {
 	private List<StationType> stationsTypes = new LinkedList<>();
 	private List<Line> lines = new LinkedList<>();
 	private Inventory inventory;
+	private boolean haveChanged;
 
 	private final EventListener<LineCreationEvent> onLineCreationEvent = event -> {
 		for(Line line : this.lines) {
@@ -75,6 +73,8 @@ public class GameMap {
 				break;
 			}
 		}
+		haveChanged = true;
+		calculateChangementPath();
 	};
 
 	private final EventListener<LineInnerExtensionEvent> onLineInnerExtensionEvent = event -> {
@@ -84,6 +84,8 @@ public class GameMap {
 				break;
 			}
 		}
+		haveChanged = true;
+		calculateChangementPath();
 	};
 
 	private final EventListener<LineDecreaseEvent> onLineDecreaseEvent = event -> {
@@ -98,7 +100,7 @@ public class GameMap {
 	private double[][] stationHeuristics;
 
     /**
-     * Default Constructor.
+     * Default Constructor
      */
     public GameMap(Inventory inventory){
         this.inventory = inventory;
@@ -106,6 +108,7 @@ public class GameMap {
 	    EventDispatcher.getInstance().addListener(LineExtensionEvent.class, this.onLineExtensionEvent);
 	    EventDispatcher.getInstance().addListener(LineInnerExtensionEvent.class, this.onLineInnerExtensionEvent);
 	    EventDispatcher.getInstance().addListener(LineDecreaseEvent.class, this.onLineDecreaseEvent);
+	    haveChanged = false;
     }
 
 	Passenger addPassengerToStation() {
@@ -118,16 +121,29 @@ public class GameMap {
 			wantedStationType = this.stationsTypes.get(random.nextInt(this.stationsTypes.size()));
 		}
 		Passenger passenger = new Passenger(station, wantedStationType);
+		passenger.setPath(pathFinding(station, wantedStationType));
+
+		//passenger.setPath();
 		station.addPassenger(passenger);
 		//passenger.setPath(findShortestPath(station, wantedStationType)); //TODO
 		return passenger;
 	}
 
 	private void initLine(Line line) {
+    	haveChanged = true;
 		Train train = this.inventory.takeTrain();
 		if(train != null) {
 			train.start(line);
 			this.addTrain(train);
+		}
+		calculateChangementPath();
+	}
+
+	private void calculateChangementPath(){
+		for(Station station : stations){
+			for(Passenger passenger : station.getPassengers()){
+				passenger.setPath(pathFinding(station, passenger.getWantedStation()));
+			}
 		}
 	}
 
@@ -214,12 +230,12 @@ public class GameMap {
      */
     @Nullable
     private Station getStationWithId(int id){
-	    for(Station station : this.stations) {
-		    if(station.gameId == id) {
-			    return station;
-		    }
-	    }
-	    return null;
+        for(Station station : stations){
+            if(station.gameId == id){
+                return station;
+            }
+        }
+        return null;
     }
 
     /**
@@ -229,12 +245,12 @@ public class GameMap {
      * @return the line with the id, null if the line with this id isn't in the map
      */
     private Line getLineWithId(int id){
-	    for(Line line : this.lines) {
-		    if(line.gameId == id) {
-			    return line;
-		    }
-	    }
-	    return null;
+        for(Line line : lines){
+            if(line.gameId == id){
+                return line;
+            }
+        }
+        return null;
     }
 
     private Connection getConnectionInStationWithLine(Station station, Line line){
@@ -255,9 +271,8 @@ public class GameMap {
             } else {
                 return null;
             }
-        } else {
-            return null;
         }
+        return null;
     }
 
     private Section getSectionWithConnections(Connection connectionA, Connection connectionB){
@@ -284,231 +299,121 @@ public class GameMap {
     }
 
     //all function for path finding (not Optimized)
-	//TODO: documentation
-
-    /**
-     * find the shortest path between a station and a type of station
-     *
-     * @param stationA      the station where we begin
-     * @param stationWanted the type of station we want
-     * @return the shortest path between the station and the type of station wanted, null if the station doesn't have connection or we don't have the type of station
-     */
-    public Stack<Station> findShortestPath(Station stationA, StationType stationWanted) {
-        List<Station> predecessor = dijkstra(stationA);
-	    Stack<Station> stationsSearching = null;
-	    this.stationHeuristics = new double[this.stations.size()][];
-	    Station stationTmp = findTheNearestStationWithType(stationA, stationWanted);
-	    if(predecessor != null) {
-		    stationsSearching = new Stack<>();
-		    while(predecessor.size() != 0) {
-			    stationsSearching.push(stationTmp);
-			    stationTmp = predecessor.get(this.stations.indexOf(stationTmp));
-			    predecessor.remove(stationTmp);
-		    }
-        }
-        return stationsSearching;
-    }
-
-    /**
-     * initialize the heuristique value for path finding
-     *
-     * @param stationA the station wher we begin
-     */
-    private void init_Dijkstra(Station stationA){
-	    int index = this.stations.indexOf(stationA);
-	    this.stationHeuristics[index] = new double[this.stations.size()];
-	    for(int i = 0; i < this.stations.size(); ++i) {
-		    if(stationA != this.stations.get(i)) {
-			    this.stationHeuristics[index][i] = Double.MAX_VALUE;
-		    }
-		    else {
-			    this.stationHeuristics[index][i] = 0;
-		    }
-        }
-    }
-
-    /**
-     * search the nearest Station near the station sent
-     * use for dijkstra Algorithm
-     *
-     * @param station the station where we are
-     * @return the nearest station of the station sent
-     */
-    private Station searchNearestStation(Station station, List<Station> stationsNotVisited){
-        double mini = Double.MAX_VALUE;
-        Station result = null;
-        //we verify that the station owns connections
-        if(station.getConnections()!=null) {
-            for (Connection connection : station.getConnections()) {
-                //we get the section which link the station and the temporary station
-	            Section section = connection.getSubSections().getRight().getSection();
-	            //we verify section isn't null
-	            if(section != null) {
-		            double tmp = section.getLength();
-		            Station station1 = section.getRightConnection().getStation();
-		            //we verify if tmp is different from 0 and we are not passed by this station before
-		            if(tmp != 0 && !stationsNotVisited.contains(section.getRightConnection().getStation())) {
-			            int index = this.stations.indexOf(station);
-			            int index2 = this.stations.indexOf(station1);
-			            //if mini is superior als the distance between station A and the temporary station
-			            if(mini > this.stationHeuristics[index][index2]) {
-				            //we save the temporary station inside result
-				            result = section.getRightConnection().getStation();
-				            //we affect mini with the good value
-				            mini = this.stationHeuristics[index][index2];
-			            }
-                    }
-                }
-            }
-        }
-        return result;
-    }
-
-    /**
-     * update the distance inside the stationsHeuristique between two stations (MUST BE CONNECTED)
-     *
-     * @param stationA the station A
-     * @param stationB the station B
-     * @param beginStation the station where we begin the dijkstra algorithm
-     * @param predecessor the list of all station we pass
-     */
-    private void updateDistance(Station stationA, Station stationB, Station beginStation, List<Station> predecessor){
-        double i = distanceBetweenTwoStationConnected(stationA, stationB);
-	    int index = this.stations.indexOf(stationA);
-	    int index1 = this.stations.indexOf(stationB);
-	    int index2 = this.stations.indexOf(beginStation);
-	    if(i >= 0) {
-		    if(this.stationHeuristics[index2][index1] > this.stationHeuristics[index2][index] + i) {
-			    this.stationHeuristics[index2][index1] = this.stationHeuristics[index2][index] + i;
-			    predecessor.add(index1, stationA);
-		    }
-        }
-    }
-
-    /**
-     * compute all the distance between all the station and the station where we are
-     *
-     * @param stationA the station where we are
-     * @return the list of station corresponding of the predecessor of all the station
-     */
-    private List<Station> dijkstra(Station stationA){
-
-        //initialisation of all the variables needed
-	    List<Station> stationsSearching = new ArrayList<>(this.stations);
-	    List<Station> predecessor = new ArrayList<>(stationsSearching.size());
-	    Station stationTmp = stationA;
-	    Station stationPredecessor = null;
-
-        //initialisation of dijkstra
-        init_Dijkstra(stationA);
-
-        //verification that stationA owns connections
-        if(stationA.getConnections() != null) {
-
-            //while the list of station isn't empty
-            while (!stationsSearching.isEmpty()) {
-
-                //find the nearest station
-                stationPredecessor = stationTmp;
-                stationTmp = searchNearestStation(stationTmp, stationsSearching);
-
-                //verify that we will not enter in a infinity loop
-                if (stationTmp == null && stationPredecessor != stationA) {
-                    //return to the beginning
-                    stationTmp = stationA;
-                } else if (stationTmp == null) {
-                    //no more stations connected so break
-                    break;
-                } else {
-                    //we remove the station finded and we update the distance for all the station neared
-                    stationsSearching.remove(stationTmp);
-                    for (Connection connection : stationTmp.getConnections()) {
-                        //we verify if we have a section and so a station
-	                    if(connection.getRightSubSection().getSection() != null) {
-		                    updateDistance(stationTmp,
-		                      connection.getRightSubSection().getSection().getRightConnection().getStation(),
-		                      stationA,
-		                      predecessor);
-	                    }
-                    }
-                }
-            }
-        }
-        return predecessor;
-    }
-
-    /**
-     * find all the station with the type wanted
-     *
-     * @param stationType the type of station we want
-     * @return the list of station with the good type
-     */
-    private List<Station> findAllStationWithType(StationType stationType){
-        List<Station> stationList = new ArrayList<>();
-	    for(Station station : this.stations) {
-		    if(station.getType() == stationType) {
-			    stationList.add(station);
-		    }
-        }
-        return stationList;
-    }
-
-    /**
-     * find the nearest station with the types wanted
-     * must be create after the initialisation of stationHeuristics
-     *
-     * @param stationA the station where we are (just for verification)
-     * @param stationType the type of station we want
-     * @return the nearest station
-     */
-    private Station findTheNearestStationWithType(Station stationA, StationType stationType){
-        double distanceTmp = Double.MAX_VALUE;
-	    int index = this.stations.indexOf(stationA);
-	    Station stationTmp = stationA;
-	    //we initialize the list which will contain all station with the good type
-	    List<Station> stationsWithGoodType = findAllStationWithType(stationType);
-	    //we verify the station where we are isn't a station with the good type
-	    if(stationA.getType() != stationType) {
-		    for(Station elem : stationsWithGoodType) {
-		        //we get the index of the station  inside the list of all stations
-			    int index1 = this.stations.indexOf(elem);
-			    //if the distance between the station of beginning and the temporary station is smaller als the distance we have
-			    if(this.stationHeuristics[index][index1] < distanceTmp) {
-				    stationTmp = elem;
-				    distanceTmp = this.stationHeuristics[index][index1];
-			    }
-            }
-        }
-        return stationTmp;
-    }
 
     /**
      * compute the distance between two station connected
+	 * Recursive
      *
      * @param stationA the first station
      * @param stationB the second station
      * @return the distance between the two station, -1 if the two station isn't connected
      */
-    private double distanceBetweenTwoStationConnected(Station stationA, Station stationB){
-        int i = 0;
-	    while(i < stationA.getConnections().size() && stationA.getConnections()
-	      .get(i)
-	      .getRightSubSection()
-	      .getSection()
-	      .getRightConnection() != null
-	      && stationA.getConnections()
-	      .get(i)
-	      .getRightSubSection()
-	      .getSection()
-	      .getRightConnection()
-	      .getStation() != stationB) {
-		    ++i;
-	    }
-        if(i < stationA.getConnections().size()) {
-	        return stationA.getConnections().get(i).getRightSubSection().getSection().getLength();
-        } else {
-            return -1;
-        }
+    private synchronized double distanceBetweenTwoStationConnected(Station stationA, Station stationB){
+    	if(stationA != stationB) {
+			for (Connection connection : stationA.getConnections()) {
+				if (connection.getLeftSubSection() != null && connection.getLeftSubSection().getSection().getOtherStationConnection(connection).getStation() == stationB) {
+					return connection.getLeftSubSection().getSection().getLength();
+				}
+				if (connection.getRightSubSection() != null && connection.getRightSubSection().getSection().getOtherStationConnection(connection).getStation() == stationB) {
+					return connection.getRightSubSection().getSection().getLength();
+				}
+			}
+			return -1;
+		}
+		return 0;
     }
 
+    private synchronized void initHeuristics(Station station){
+    	int index = stations.indexOf(station);
+    	int i = 0;
+    	stationHeuristics = new double[stations.size()][stations.size()];
+		for (Station station1 : stations) {
+			if (station1 == station) {
+				stationHeuristics[index][i] = 0;
+			} else {
+				stationHeuristics[index][i] = Double.MAX_VALUE;
+			}
+			++i;
+		}
+	}
+
+	/**
+	 * update the distance inside the stationsHeuristique between two stations (MUST BE CONNECTED)
+	 *
+	 * @param stationA the station A
+	 * @param stationB the station B
+	 * @param beginStation the station where we begin
+	 */
+	private void updateDistance(Station stationA, Station stationB, Station beginStation, Station[] predecessor){
+		double i = distanceBetweenTwoStationConnected(stationA, stationB);
+		int index = this.stations.indexOf(stationA);
+		int index1 = this.stations.indexOf(stationB);
+		int index2 = this.stations.indexOf(beginStation);
+		if(i >= 0) {
+			if(this.stationHeuristics[index2][index1] > this.stationHeuristics[index2][index] + i) {
+				this.stationHeuristics[index2][index1] = this.stationHeuristics[index2][index] + i;
+				predecessor[index1] = stationA;
+			}
+		}
+	}
+
+	/**
+	 * path finding for the passenger
+	 * Not Optimized
+	 *
+	 * @param station the station of the passenger
+	 * @param stationType the type of station wanted
+	 * @return the path
+	 */
+    public synchronized Stack<Station> pathFinding(Station station, StationType stationType){
+		System.out.println("Wanted type = " + stationType + " and station is " + station.toString());
+		Stack<Station> result = new Stack<>();
+		Station[] predecessor = new Station[stations.size()];
+		Station current = station;
+
+		initHeuristics(station);
+		if(station.getType() != stationType) {
+			PriorityQueue<Station> path = new PriorityQueue<>(new Comparator<Station>() {
+				@Override
+				public int compare(Station o1, Station o2) {
+					return (int) (stationHeuristics[stations.indexOf(station)][stations.indexOf(o1)] -
+							stationHeuristics[stations.indexOf(station)][stations.indexOf(o2)]);
+				}
+			});
+			path.add(station);
+			Stack<Station> finished = new Stack<>();
+			predecessor[stations.indexOf(station)] = current;
+			while (!path.isEmpty()){
+				System.out.println(path.toString());
+				current = path.poll();
+				if(current.getType() == stationType){
+					System.out.println("Waiting Result");
+					while (current != station){
+						int index = stations.indexOf(current);
+						result.push(current);
+						current = predecessor[index];
+					}
+					System.out.println("Path finded : " + result.toString());
+					return result;
+				}
+				for(Connection connection : current.getConnections()){
+					if(connection.getRightSubSection() != null &&
+							!finished.contains(connection.getRightSubSection().getSection().getOtherStationConnection(connection).getStation())){
+						updateDistance(connection.getStation(), connection.getRightSubSection().getSection().getOtherStationConnection(connection).getStation(),station, predecessor);
+						path.add(connection.getRightSubSection().getSection().getOtherStationConnection(connection).getStation());
+					}
+					if(connection.getLeftSubSection() != null &&
+							!finished.contains(connection.getLeftSubSection().getSection().getOtherStationConnection(connection).getStation())){
+						updateDistance(connection.getStation(), connection.getLeftSubSection().getSection().getOtherStationConnection(connection).getStation(), station, predecessor);
+						path.add(connection.getLeftSubSection().getSection().getOtherStationConnection(connection).getStation());
+					}
+				}
+				finished.add(current);
+			}
+		} else{
+			System.out.println("The station is of type wanted");
+		}
+		System.out.println("No path finded");
+		return null;
+	}
 }
